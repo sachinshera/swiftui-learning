d240d46e315752a5a57e87149278ccdf76436127
sachin kumar <sachinkumarshera@gmail.com>
2023-12-28 15:02:12 +0530

    Initial Commit

---
+++ bootcamp/bootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  bootcampApp.swift
+//  bootcamp
+//
+//  Created by SACHIN KUMAR on 28/12/23.
+//
+
+import SwiftUI
+
+@main
+struct bootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
